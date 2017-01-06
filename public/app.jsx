858afa8545356f447369236ec94ab3f1ead3cf4d
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-06 21:03:37 +1300

    default and props working

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,24 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message'
+    }
+  },
   render: function () {
+    var name = this.props.name
+    var message = this.props.message
     return (
       <div>
-        <h1>Hello React</h1>
+        <h1>Hello {name}</h1>
+        <p>{message}</p>
       </div>
     )
   }
 })
-
+ var firtsName = 'Rodrigo'
+ var message = 'This is a message'
   ReactDOM.render(
-    <Greeter />,
+    <Greeter name={firtsName} message={message}/>,
     document.getElementById('app')
   )
