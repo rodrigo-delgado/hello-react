c9759176c550d9bbb070d413cd94b13ea03879bf
rodrigo-delgado <rod.delgado85@gmail.com>
2017-01-07 16:08:08 +1300

    webpack config file done

--- public/app.jsx
+++ public/app.jsx
@@ -1,12 +1,12 @@
 //Every react Component needs a render method
 var React = require('react')
 var ReactDOM = require('react-dom')
-var Greeter = require('./components/Greeter')
+var Greeter = require('Greeter')
 //GreeterForm Component
 
 
  var firtsName = 'Rodrigo'
- 
+
   ReactDOM.render(
     <Greeter name={firtsName} />,
     document.getElementById('app')
--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,6 +1,6 @@
 var React = require('react')
-var GreeterMessage = require('./GreeterMessage')
-var GreeterForm = require('./GreeterForm')
+var GreeterMessage = require('GreeterMessage')
+var GreeterForm = require('GreeterForm')
 
 //Greeter Component
 var Greeter = React.createClass({
